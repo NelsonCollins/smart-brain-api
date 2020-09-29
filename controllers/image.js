292036843db917ceac8b71dfbceec67199921fc5
NelsonCollins <nelsoncollins90@gmail.com>
2020-09-29 11:01:54 +0100

    Nodemon error commit

--- controllers/image.js
+++ controllers/image.js
@@ -6,7 +6,7 @@ const app = new Clarifai.App({
 const handleApicall = (req,res) =>{
 	app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
 	.then(data =>{
-		res.json(data);
+		res.json(data)
 	})
 	.catch(err => res.status(400).json('Unable to work with API'));
 }
